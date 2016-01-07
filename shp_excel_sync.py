24741827072f4b909b0051bf913f1f940fb90002
Matthias Kuhn <matthias@opengis.ch>
2016-01-07 15:39:17 +0100

    Various updates:
     * id as integer
     * X and Y in separate columns
     * Ignore NULL values reported from XLS

--- shp_excel_sync.py
+++ shp_excel_sync.py
@@ -1,5 +1,4 @@
 from sets import Set
-from datetime import datetime
 
 from qgis._core import QgsMessageLog, QgsMapLayerRegistry, QgsFeatureRequest, QgsFeature
 from qgis.utils import iface
@@ -33,18 +32,19 @@ logTag="OpenGIS" # in which tab log messages appear
 # excel layer
 excelName="Excel"# the layer name
 excelSheetName="Tabelle1"
-excelFkIdx = 1
-excelCentroidIdx = 70
-excelAreaIdx = 8
+excelFkIdx = 0
+excelCentroidxIdx = 71
+excelCentroidyIdx = 72
+excelAreaIdx = 9
 excelPath=layer_from_name(excelName).publicSource()
 excelKeyName = field_name_from_idx(excelName, excelFkIdx)
 # shpfile layer
 shpName="Massnahmepool"
-shpKeyName="ef_key"
+shpKeyName="id"
 shpKeyIdx = field_idx_from_name(shpName, shpKeyName)
 
 # non configurable - no edits beyond this point
-skipFirstLineExcel = True
+skipFirstLineExcel = 1
 
 # state variables
 filewatcher=None
@@ -55,7 +55,10 @@ shpRemove = Set([])
 def showWarning(msg):
     QtGui.QMessageBox.information(iface.mainWindow(),'Warning',msg)
 
-def get_fk_set(layerName, fkName, skipFirst=True, fids=None, useProvider=False):
+def get_fk_set(layerName, fkName, skipFirst=1, fids=None, useProvider=False):
+    """
+        skipFirst: number of initial lines to skip (header lines in excel)
+    """
     layer = layer_from_name(layerName)
     freq = QgsFeatureRequest()
     if fids is not None:
@@ -65,13 +68,11 @@ def get_fk_set(layerName, fkName, skipFirst=True, fids=None, useProvider=False):
     else:
         feats = [f for f in layer.dataProvider().getFeatures(freq)]
     fkSet = []
-    first=True
-    for f in feats:
-        if skipFirst and first:
-            first=False
-            continue
+    for f in feats[skipFirst:]:
+        QgsMessageLog.logMessage('FK {}'.format(f.attribute(fkName)), logTag, QgsMessageLog.CRITICAL)
         fk = f.attribute(fkName)
-        fkSet.append(fk)
+        if fk: # Skip NULL ids that may be reported from excel files
+            fkSet.append(fk)
     return fkSet
 
 def info(msg):
@@ -110,8 +111,7 @@ def excel_changed():
 
 def get_max_id():
     layer = layer_from_name(shpName)
-    vals = layer.uniqueValues(shpKeyIdx) # it returns those as strings :(
-    return max(map(long,vals))
+    return layer.maximumValue(shpKeyIdx)
 
 def added_geom(layerId, feats):
     # unused for now
@@ -121,7 +121,7 @@ def added_geom(layerId, feats):
     for i,_ in enumerate(feats):
         _id = maxFk + i + 1
         feats[i].setAttribute(shpKeyName,_id)
-        res = layer.changeAttributeValue(feats[i].id(),shpKeyIdx,str(_id))
+        res = layer.changeAttributeValue(feats[i].id(),shpKeyIdx,_id)
 
     global shpAdd
     shpAdd = feats
@@ -136,6 +136,11 @@ def query_layer_for_fids(layerName, fids):
 
 
 def added_geom_precommit(fid):
+    # Only when features are added to the edit buffer and not after commit.
+    # Temporary features have fid < 0
+    if fid > 0:
+        return
+
     # TODO: Buggy if I add something and then i delete it
     #info("precomit fid"+str(fid))
     global shpAdd
@@ -144,23 +149,15 @@ def added_geom_precommit(fid):
     _id = maxFk + len(shpAdd) + 1
     feat = query_layer_for_fids(shpName, [fid])[0]
 
-    # unfortunately the way/order signals work, 
-    # it may be attempted to add something twice, so check that geometries are unique
-    for f in shpAdd:
-        #info("shpadd:"+str(f.geometry().exportToWkt())+" vs "+str(feat.geometry().exportToWkt()))
-        # plain geometry equality check seems to not work
-        if f.geometry().exportToWkt() == feat.geometry().exportToWkt():
-            #info("will skip this one")
-            return
-
     feat.setAttribute(shpKeyName,_id)
-    res = layer.changeAttributeValue(feat.id(),shpKeyIdx,str(_id))
+    res = layer.changeAttributeValue(feat.id(),shpKeyIdx,_id)
+    info('Set new id for feature {}: {}'.format(feat.id(), _id))
     shpAdd.append(feat)
 
 
 def removed_geom_precommit(fids):
     #info("Removed fids"+str(fids))
-    fks_to_remove = get_fk_set(shpName,shpKeyName,skipFirst=False,fids=fids,useProvider=True)
+    fks_to_remove = get_fk_set(shpName,shpKeyName,skipFirst=0,fids=fids,useProvider=True)
     global shpRemove
     shpRemove = shpRemove.union(fks_to_remove)
     info("feat ids to remove"+str(shpRemove))
@@ -168,7 +165,7 @@ def removed_geom_precommit(fids):
 def changed_geom(layerId, geoms):
     fids = geoms.keys()
     feats = query_layer_for_fids(shpName, fids)
-    fks_to_change = get_fk_set(shpName,shpKeyName,skipFirst=False,fids=fids)
+    fks_to_change = get_fk_set(shpName,shpKeyName,skipFirst=0,fids=fids)
     global shpChange
     shpChange = {k:v for (k,v) in zip(fks_to_change, feats)}
     #info("changed"+str(shpChange))
@@ -176,9 +173,11 @@ def changed_geom(layerId, geoms):
 
 def write_feature_to_excel(sheet, idx, feat):
    area = feat.geometry().area()*0.0001 # Square meters to hectare
-   centroid = str(feat.geometry().centroid().asPoint())
+   centroidx = str(feat.geometry().centroid().asPoint().x())
+   centroidy = str(feat.geometry().centroid().asPoint().y())
    sheet.write(idx, excelFkIdx, feat[shpKeyName])
-   sheet.write(idx, excelCentroidIdx, centroid)
+   sheet.write(idx, excelCentroidxIdx, centroidx)
+   sheet.write(idx, excelCentroidyIdx, centroidy)
    sheet.write(idx, excelAreaIdx, area)
 
 def write_rowvals_to_excel(sheet, idx, vals, ignore=None):
@@ -209,8 +208,8 @@ def update_excel_programmatically():
             continue         
         #print(r_sheet.cell(row_index,1).value)
         fk = r_sheet.cell(row_index, excelFkIdx).value
-        maxFk = max(maxFk, long(fk))
-        fk = str(long(fk)) # FIXME: Why do we have the keys as strings from the shp?
+        maxFk = max(maxFk, fk)
+        # fk = str(long(fk)) # FIXME: Why do we have the keys as strings from the shp?
         if fk in shpRemove:
             status_msgs.append("Removing feature with id {}".format(fk))
             continue
@@ -259,7 +258,7 @@ def updateShpLayer(fksToRemove):
     if not fksToRemove:
         return
 
-    prompt_msg = "Attempt to synchronize between Excel and Shapefile. Shapefile has features with ids: ({}) that don't appear in the Excel. Delete those features from the shapefile? ".format(','.join(fksToRemove))
+    prompt_msg = "Attempt to synchronize between Excel and Shapefile. Shapefile has features with ids: ({}) that don't appear in the Excel. Delete those features from the shapefile? ".format(','.join([str(fk) for fk in fksToRemove]))
     reply = QtGui.QMessageBox.question(iface.mainWindow(), 'Message', 
                      prompt_msg, QtGui.QMessageBox.Yes, QtGui.QMessageBox.No)
 
@@ -280,7 +279,7 @@ def update_shp_from_excel():
     if not excelFks:
         warn("Qgis thinks that the Excel file is empty. That probably means something went horribly wrong. Won't sync.")
         return
-    shpFks = Set(get_fk_set(shpName,shpKeyName,skipFirst=False))
+    shpFks = Set(get_fk_set(shpName,shpKeyName,skipFirst=0))
     # TODO also special warning if shp layer is in edit mode
     info("Keys in excel"+str(excelFks))
     info("Keys in shp"+str(shpFks))
