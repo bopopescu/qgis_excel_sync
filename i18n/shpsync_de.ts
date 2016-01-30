01015abc3589860d510ecb4d29f9f15a7d54c232
carolinux <karolina.alexiou@teralytics.ch>
2016-01-30 14:53:29 +0100

    Sets up translation

---
+++ i18n/shpsync_de.ts
@@ -0,0 +1,51 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="de_DE">
+<context>
+    <name>shpsyncDialogBase</name>
+    <message>
+        <source>ShpSync</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Master Layer:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Master Key:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Excel Layer:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Excel Key:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Excel Sheet:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Skip lines:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>                                   Derived fields</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Excel Field</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Expression on Shapefile</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Add more derived fields</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ shpsync.pro
@@ -0,0 +1,7 @@
+
+SOURCES = shpsync_dialog_base.ui
+
+TRANSLATIONS = TRANSLATIONS = i18n/shpsync_de.ts \
+i18n/shpsync_fr.ts
+
+RESOURCES = resources.qrc
