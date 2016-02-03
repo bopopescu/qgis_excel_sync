d257fd07f04e48390e591c5f7efe70bb3c663255
Matthias Kuhn <matthias@opengis.ch>
2016-02-03 13:12:26 +0100

    More german translation

--- i18n/de.ts
+++ i18n/de.ts
@@ -1,49 +1,88 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.0" language="de_DE">
+<!DOCTYPE TS><TS version="2.0" language="de_DE" sourcelanguage="">
+<context>
+    <name>@default</name>
+    <message>
+        <location filename="test_translations.py" line="48"/>
+        <source>Good morning</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>shpsync</name>
+    <message>
+        <location filename="shpsync.py" line="183"/>
+        <source>&amp;ShpSync</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="shpsync.py" line="174"/>
+        <source>Set up ShpSync</source>
+        <translation>ShpSync einrichten</translation>
+    </message>
+</context>
+<context>
+    <name>shpsyncDialog</name>
+    <message>
+        <location filename="shpsync_dialog.py" line="69"/>
+        <source>Delete</source>
+        <translation>Löschen</translation>
+    </message>
+</context>
 <context>
     <name>shpsyncDialogBase</name>
     <message>
+        <location filename="shpsync_dialog_base.ui" line="14"/>
         <source>ShpSync</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="shpsync_dialog_base.ui" line="23"/>
         <source>Master Layer:</source>
         <translation>Hauptebene</translation>
     </message>
     <message>
+        <location filename="shpsync_dialog_base.ui" line="33"/>
         <source>Master Key:</source>
         <translation>Identifikationsschlüssel Hauptebene</translation>
     </message>
     <message>
+        <location filename="shpsync_dialog_base.ui" line="43"/>
         <source>Excel Layer:</source>
         <translation>Excelebene</translation>
     </message>
     <message>
+        <location filename="shpsync_dialog_base.ui" line="53"/>
         <source>Excel Key:</source>
         <translation>Identifikationsschlüssel Excelebene</translation>
     </message>
     <message>
+        <location filename="shpsync_dialog_base.ui" line="63"/>
         <source>Excel Sheet:</source>
         <translation>Excel Datenblatt</translation>
     </message>
     <message>
+        <location filename="shpsync_dialog_base.ui" line="73"/>
         <source>Skip lines:</source>
         <translation>Zeilen überspringen</translation>
     </message>
     <message>
+        <location filename="shpsync_dialog_base.ui" line="94"/>
         <source>Derived fields</source>
         <translation>Berechnete Felder</translation>
     </message>
     <message>
+        <location filename="shpsync_dialog_base.ui" line="115"/>
         <source>Excel Field</source>
         <translation>Excel Feld</translation>
     </message>
     <message>
+        <location filename="shpsync_dialog_base.ui" line="122"/>
         <source>Expression on Shapefile</source>
         <translation>Ausdruck (auf Shapefile)</translation>
     </message>
     <message>
+        <location filename="shpsync_dialog_base.ui" line="103"/>
         <source>Add more derived fields</source>
         <translation>Mehr berechnete Felder hinzufügen</translation>
     </message>
