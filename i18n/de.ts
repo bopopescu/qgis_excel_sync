2d86372e3fd0820d404dcf46f19a2f0a373bef4d
Matthias Kuhn <matthias@opengis.ch>
2019-05-05 11:30:47 +0200

    Translation update

--- i18n/de.ts
+++ i18n/de.ts
@@ -1,24 +1,96 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="2.0" language="de_DE" sourcelanguage="">
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
 <context>
-    <name>@default</name>
+    <name>excel_sync</name>
     <message>
-        <location filename="test_translations.py" line="48"/>
-        <source>Good morning</source>
-        <translation type="unfinished"></translation>
+        <location filename="../excel_sync/excel_sync.py" line="199"/>
+        <source>&amp;ExcelSync</source>
+        <translation>&amp;ExcelSync</translation>
+    </message>
+    <message>
+        <location filename="../excel_sync/excel_sync.py" line="190"/>
+        <source>Set up ExcelSync</source>
+        <translation>ExcelSync konfigurieren</translation>
+    </message>
+</context>
+<context>
+    <name>excel_syncDialog</name>
+    <message>
+        <location filename="../excel_sync/gui/excel_sync_dialog.py" line="101"/>
+        <source>Delete</source>
+        <translation>Löschen</translation>
+    </message>
+</context>
+<context>
+    <name>excel_syncDialogBase</name>
+    <message>
+        <location filename="../excel_sync/gui/excel_sync_dialog_base.ui" line="14"/>
+        <source>ExcelSync</source>
+        <translation>ExcelSync Konfiguration</translation>
+    </message>
+    <message>
+        <location filename="../excel_sync/gui/excel_sync_dialog_base.ui" line="23"/>
+        <source>Master Layer:</source>
+        <translation>Geometrieebene</translation>
+    </message>
+    <message>
+        <location filename="../excel_sync/gui/excel_sync_dialog_base.ui" line="33"/>
+        <source>Master Key:</source>
+        <translation>Identifikationsschlüssel Geometrieebene</translation>
+    </message>
+    <message>
+        <location filename="../excel_sync/gui/excel_sync_dialog_base.ui" line="43"/>
+        <source>Excel Layer:</source>
+        <translation>Excelebene</translation>
+    </message>
+    <message>
+        <location filename="../excel_sync/gui/excel_sync_dialog_base.ui" line="53"/>
+        <source>Excel Key:</source>
+        <translation>Identifikationsschlüssel Excelebene</translation>
+    </message>
+    <message>
+        <location filename="../excel_sync/gui/excel_sync_dialog_base.ui" line="63"/>
+        <source>Excel Sheet:</source>
+        <translation>Excel Datenblatt</translation>
+    </message>
+    <message>
+        <location filename="../excel_sync/gui/excel_sync_dialog_base.ui" line="73"/>
+        <source>Skip lines:</source>
+        <translation>Zeilen überspringen</translation>
+    </message>
+    <message>
+        <location filename="../excel_sync/gui/excel_sync_dialog_base.ui" line="87"/>
+        <source>Derived fields</source>
+        <translation>Berechnete Felder</translation>
+    </message>
+    <message>
+        <location filename="../excel_sync/gui/excel_sync_dialog_base.ui" line="96"/>
+        <source>Add more derived fields</source>
+        <translation>Weitere berechnete Felder hinzufügen</translation>
+    </message>
+    <message>
+        <location filename="../excel_sync/gui/excel_sync_dialog_base.ui" line="108"/>
+        <source>Excel Field</source>
+        <translation>Excel Feld</translation>
+    </message>
+    <message>
+        <location filename="../excel_sync/gui/excel_sync_dialog_base.ui" line="115"/>
+        <source>Expression on Shapefile</source>
+        <translation>Ausdruck (auf Shapefile)</translation>
+    </message>
+    <message>
+        <location filename="../excel_sync/gui/excel_sync_dialog_base.ui" line="144"/>
+        <source>Hide Add Feature Dialog</source>
+        <translation>Feature Hinzufügen Dialog verstecken</translation>
     </message>
 </context>
 <context>
     <name>shpsync</name>
     <message>
-        <location filename="shpsync.py" line="183"/>
-        <source>&amp;ShpSync</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
         <location filename="shpsync.py" line="174"/>
         <source>Set up ShpSync</source>
-        <translation>ShpSync einrichten</translation>
+        <translation type="obsolete">ShpSync einrichten</translation>
     </message>
 </context>
 <context>
@@ -26,65 +98,60 @@
     <message>
         <location filename="shpsync_dialog.py" line="69"/>
         <source>Delete</source>
-        <translation>Löschen</translation>
+        <translation type="obsolete">Löschen</translation>
     </message>
 </context>
 <context>
     <name>shpsyncDialogBase</name>
     <message>
-        <location filename="shpsync_dialog_base.ui" line="14"/>
-        <source>ShpSync</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
         <location filename="shpsync_dialog_base.ui" line="23"/>
         <source>Master Layer:</source>
-        <translation>Geometrieebene</translation>
+        <translation type="obsolete">Geometrieebene</translation>
     </message>
     <message>
         <location filename="shpsync_dialog_base.ui" line="33"/>
         <source>Master Key:</source>
-        <translation>Identifikationsschlüssel Geometrieebene</translation>
+        <translation type="obsolete">Identifikationsschlüssel Geometrieebene</translation>
     </message>
     <message>
         <location filename="shpsync_dialog_base.ui" line="43"/>
         <source>Excel Layer:</source>
-        <translation>Excelebene</translation>
+        <translation type="obsolete">Excelebene</translation>
     </message>
     <message>
         <location filename="shpsync_dialog_base.ui" line="53"/>
         <source>Excel Key:</source>
-        <translation>Identifikationsschlüssel Excelebene</translation>
+        <translation type="obsolete">Identifikationsschlüssel Excelebene</translation>
     </message>
     <message>
         <location filename="shpsync_dialog_base.ui" line="63"/>
         <source>Excel Sheet:</source>
-        <translation>Excel Datenblatt</translation>
+        <translation type="obsolete">Excel Datenblatt</translation>
     </message>
     <message>
         <location filename="shpsync_dialog_base.ui" line="73"/>
         <source>Skip lines:</source>
-        <translation>Zeilen überspringen</translation>
+        <translation type="obsolete">Zeilen überspringen</translation>
     </message>
     <message>
         <location filename="shpsync_dialog_base.ui" line="94"/>
         <source>Derived fields</source>
-        <translation>Berechnete Felder</translation>
+        <translation type="obsolete">Berechnete Felder</translation>
     </message>
     <message>
         <location filename="shpsync_dialog_base.ui" line="115"/>
         <source>Excel Field</source>
-        <translation>Excel Feld</translation>
+        <translation type="obsolete">Excel Feld</translation>
     </message>
     <message>
         <location filename="shpsync_dialog_base.ui" line="122"/>
         <source>Expression on Shapefile</source>
-        <translation>Ausdruck (auf Shapefile)</translation>
+        <translation type="obsolete">Ausdruck (auf Shapefile)</translation>
     </message>
     <message>
         <location filename="shpsync_dialog_base.ui" line="103"/>
         <source>Add more derived fields</source>
-        <translation>Mehr berechnete Felder hinzufügen</translation>
+        <translation type="obsolete">Mehr berechnete Felder hinzufügen</translation>
     </message>
 </context>
 </TS>
